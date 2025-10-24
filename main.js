aa5bfcb179dc49344fb811a6aec0f979fbbc3d3f
201000thanhnguyen <201000thanhnguyen@gmail.com>
2025-10-24 23:37:35 +0700

    fix validate ui

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
 
   // init
+  initForm();
   const payPerShift = 75; // 75k VND per shift
   var totalPayToday = 0;
-
-  $(`#inputWorkingDays`).val(new Date().toISOString().split('T')[0]);
-
+  
   // event
   $(document).on("click", 'a[href^="#"]', function (event) {
     event.preventDefault();
@@ -25,6 +24,9 @@ $(document).ready(function () {
     const isOff = $(this).val() === 'Off';
     $('input[name="checkboxWorkShift"]').val([]);
     $('input[name="checkboxWorkShift"]').prop('disabled', isOff);
+
+    $('#alertCheckboxWorkShift').removeClass('d-inline');
+    $('#alertCheckboxWorkShift').addClass('d-none');
   });
 
   $('#btnSave').on('click', function () {
@@ -50,8 +52,9 @@ $(document).ready(function () {
     if (!checkValid) {
       return;
     }
-    
+
     console.log(obj);
+    initForm();
 
     /*
     $.ajax({
@@ -86,6 +89,14 @@ $(document).ready(function () {
     return true;
   }
 
-  // callback functions
-
 });
+
+// functions
+function initForm() {
+  $(`#inputWorkingDays`).val(new Date().toISOString().split('T')[0]);
+  $('#inputWorkingDays').attr('max', new Date().toISOString().split('T')[0]);
+  $('input[name="radioWorkStatus"][value="Working"]').prop('checked', true);
+  $('input[name="checkboxWorkShift"]').val([]);
+  $('input[name="checkboxWorkShift"]').prop('disabled', false);
+  $('#inputNote').val('');
+}
